6a317ddb266f9d7c044a9c28c52789c5152425e1
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-08-20 08:00:08 -0500

    commit tree 6d666a4a0d5caccb4f06f0f18f3dfb67604331b8

--- main.js
+++ main.js
@@ -2,7 +2,27 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// When a user clicks on an empty heart:
+// Invoke mimicServerCall to simulate making a server request
+// When the "server" returns a failure status:
+// Respond to the error using a .catch(() => {}) block after
+//  your .then(() => {}) block.
+// Display the error modal by removing the .hidden class
+// Display the server error message in the modal
+// Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+
+const emptyHearts = document.querySelectorAll("span.like-glyph");
+
+function clickHeart(arr){
+  arr.forEach(element => element.addEventListener('click', () => {
+    mimicServerCall()
+      .then(resp => console.log(resp))
+      .catch(error => console.log(error.message))
+  }))
+}
+
+clickHeart(emptyHearts)
+
 
 
 
